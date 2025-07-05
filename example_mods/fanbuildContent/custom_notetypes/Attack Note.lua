d0a9f8f052f5c1d9267658df35bf03f913e3cb79
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-05 14:39:41 +0200

    unzipgit

---
+++ example_mods/fanbuildContent/custom_notetypes/Attack Note.lua
@@ -0,0 +1,64 @@
+local luaNote = "Attack Note"
+function onCreate()
+	--Iterate over all notes
+	for i = 0, getProperty('unspawnNotes.length')-1 do
+		--Check if the note is an Instakill Note
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == luaNote then
+			--setPropertyFromGroup('unspawnNotes', i, 'texture', 'NOTE_assets'); --Change texture
+			setPropertyFromGroup('unspawnNotes', i, 'hitHealth', '0.023'); --Default value is: 0.023, health gained on hit
+			setPropertyFromGroup('unspawnNotes', i, 'missHealth', '0.0475'); --Default value is: 0.0475, health lost on miss
+			setPropertyFromGroup('unspawnNotes', i, 'hitCausesMiss', false);
+
+			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') then --Doesn't let Dad/Opponent notes get ignored
+				setPropertyFromGroup('unspawnNotes', i, 'ignoreNote', false); --Miss has no penalties
+			end
+		end
+	end
+	addCharacterToList("finn-slash", "dad")
+	addCharacterToList("finn-hurting", "dad")
+	--debugPrint('Script started!')
+end
+
+-- Function called when you hit a note (after note hit calculations)
+-- id: The note member id, you can get whatever variable you want from this note, example: "getPropertyFromGroup('notes', id, 'strumTime')"
+-- noteData: 0 = Left, 1 = Down, 2 = Up, 3 = Right
+-- noteType: The note type string/tag
+-- isSustainNote: If it's a hold note, can be either true or false
+local anims = {"LEFT", "DOWN", "UP", "RIGHT"}
+function goodNoteHit(id, noteData, noteType, isSustainNote)
+	if noteType == luaNote then
+		playAnim("boyfriend", "shoot"..anims[noteData+1])
+		setProperty("boyfriend.specialAnim", true)
+		
+		if dadName == "finn-sword" or dadName == "finn-slash" or dadName == "finn-hurting" then
+			triggerEvent("Change Character", "dad", "finn-hurting")
+			playAnim("dad", "sing"..anims[noteData])
+			setProperty("dad.specialAnim", true)
+		end
+	end
+end
+
+function noteMiss(id, direction, noteType, isSustainNote)
+	if noteType == luaNote then
+		if dadName == "finn-sword" or dadName == "finn-slash" or dadName == "finn-hurting" then
+			triggerEvent("Change Character", "dad", "finn-slash")
+		end
+
+		--playAnim("dad", "attack", true)
+		playAnim("dad", "sing"..anims[noteData], true)
+		setProperty("dad.specialAnim", true)
+		--if getModSetting("screenGlitch") then cameraShake("game", 0.1, 0.2) end
+	end
+end
+
+--[[function opponentNoteHit(id, noteData, noteType, isSustainNote)
+	if noteType == luaNote then
+		if dadName == "finn-sword" or dadName == "finn-slash" or dadName == "finn-hurting" then
+			triggerEvent("Change Character", "dad", "finn-slash")
+		end
+
+		--playAnim("dad", "attack", true)
+		playAnim("dad", "sing"..anims[noteData], true)
+		setProperty("dad.specialAnim", true)
+	end
+end--]]
