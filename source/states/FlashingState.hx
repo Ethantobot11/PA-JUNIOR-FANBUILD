3f54ae7737badff0e89fa046b92e669ff9119233
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-05 15:23:11 +0200

    unzipgit

--- source/states/FlashingState.hx
+++ source/states/FlashingState.hx
@@ -15,22 +15,24 @@ class FlashingState extends MusicBeatState
 	{
 		super.create();
 
+		final enter:String = (controls.mobileC) ? 'A' : 'ENTER';
+		final back:String = (controls.mobileC) ? 'B' : 'BACK';
+
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 
-		var guh:String = "Hey, watch out!\n
-		This Mod contains some flashing lights!\n
-		Press A/ENTER to disable them now or go to Options Menu.\n
-		Press B/ESCAPE to ignore this message.\n
-		You've been warned!";
-
-		controls.isInSubstate = false; // qhar I hate it
-		warnText = new FlxText(0, 0, FlxG.width, guh, 32);
+		warnText = new FlxText(0, 0, FlxG.width,
+			"Hey, watch out!\n
+			This Mod contains some flashing lights!\n
+			Press " + enter + " to disable them now or go to Options Menu.\n
+			Press " + back + " to ignore this message.\n
+			You've been warned!",
+			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
 
-		addTouchPad("NONE", "A_B");
+		addTouchPad('NONE', 'A_B');
 	}
 
 	override function update(elapsed:Float)
@@ -47,14 +49,14 @@ class FlashingState extends MusicBeatState
 					FlxG.sound.play(Paths.sound('confirmMenu'));
 					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
 						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
-							MusicBeatState.switchState(new TitleState());
+							FlxG.switchState(() -> new TitleState());
 						});
 					});
 				} else {
 					FlxG.sound.play(Paths.sound('cancelMenu'));
 					FlxTween.tween(warnText, {alpha: 0}, 1, {
 						onComplete: function (twn:FlxTween) {
-							MusicBeatState.switchState(new TitleState());
+							FlxG.switchState(() -> new TitleState());
 						}
 					});
 				}
